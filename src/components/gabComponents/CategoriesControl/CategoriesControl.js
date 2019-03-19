46c7c3e769ea2d2c86130f6b61f28e503afa2471
Giuseppe Iannì <giuseppe.ianni@gmail.com>
2019-03-19 16:55:44 +0100

    Add component CategoriesControl

---
+++ src/components/gabComponents/CategoriesControl/CategoriesControl.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Control from '@skyeer/react-leaflet-custom-control'
+
+const StyledWrapper = styled.div`
+  background: white;
+  border: 1px solid red;
+  padding: 10px;
+`
+
+class CategoriesControl extends Component {
+
+  render() {
+    const {
+      categories,
+    } = this.props
+    console.log(`categories from control: ${categories}`);
+
+    return(
+      <Control
+        position="topright">
+        <StyledWrapper>
+              <h2>Categories:</h2>
+              <br />
+              <ul>
+                {categories.map((item, k) => (
+                  <li key={k}>{item}</li>
+                ))}
+              </ul>
+
+        </StyledWrapper>
+      </Control>
+    )
+  }
+}
+
+export default CategoriesControl
