c4ed7cce0ca646b9222acdf1b250a3427a2fccea
Marco Martino <mecmartini@gmail.com>
2019-03-22 16:40:31 +0100

    Give category buttons a gradient background based on the percentage of the category related

--- src/components/gabComponents/CategoriesControl/CategoriesControl.js
+++ src/components/gabComponents/CategoriesControl/CategoriesControl.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 
 const StyledList = styled.ul`
@@ -6,8 +6,12 @@ const StyledList = styled.ul`
   padding: 0;
   li {
     margin-bottom: 5px;
+    background: #17bebb;
     &:last-child {
       margin-bottom: 0;
+    }
+    &.active {
+      background: #0065a2;
     }
   }
 `
@@ -15,24 +19,29 @@ const StyledList = styled.ul`
 const StyledButton = styled.button`
   border: none;
   background: #ffffff;
-  color: #0065a2;
+  color: #ffffff;
   padding: 5px;
   text-transform: uppercase;
   border: 1px solid #0065a2;
   box-sizing: border-box;
   font-weight: bold;
+  width: 100%;
+  text-align: left;
+  background: -moz-linear-gradient(left, #70b77e 0%, #70b77e ${props => props.percentage}%, transparent ${props => props.percentage + 0.1}%, transparent 100%);
+  background: -webkit-linear-gradient(left, #70b77e 0%,#70b77e ${props => props.percentage}%,transparent ${props => props.percentage + 0.1}%,transparent 100%);
+  background: linear-gradient(to right, #70b77e 0%,#70b77e ${props => props.percentage}%,transparent ${props => props.percentage + 0.1}%,transparent 100%);
   &:hover {
     cursor: pointer;
   }
   &.active,
   &.active:hover {
-    background: #0065a2;
-    color: #f1ffe7;
+    background: -moz-linear-gradient(left, #70b77e 0%, #70b77e ${props => props.percentage}%, transparent ${props => props.percentage + 0.1}%, transparent 100%);
+    background: -webkit-linear-gradient(left, #70b77e 0%,#70b77e ${props => props.percentage}%,transparent ${props => props.percentage + 0.1}%,transparent 100%);
+    background: linear-gradient(to right, #70b77e 0%,#70b77e ${props => props.percentage}%,transparent ${props => props.percentage + 0.1}%,transparent 100%);
   }
   &.cat-all {
     border: 1px solid #70b77e;
     background: #70b77e;
-    color: #ffffff;
     &.active,
     &.active:hover {
       background: #70b77e;
@@ -52,6 +61,8 @@ class CategoriesControl extends Component {
 
     let allCategoriesActive = true;
     if (categories.length) {
+      categories.sort(compare);
+
       allCategoriesActive = categories.reduce(
         (allActive, category) => {
           let active = true;
@@ -74,30 +85,40 @@ class CategoriesControl extends Component {
     }
 
     return(
-      <StyledList>
-        <li>
-          <StyledButton
-            className={allCategoriesActive ? 'cat-all active' : 'cat-all'  }
-            value="all"
-            onClick={handleCategoriesSelectAllClick}
-          >
-            Select All
-          </StyledButton>
-        </li>
-        {categories.map((item, k) => (
-          <li key={k}>
-            <StyledButton
-              className={item.status ? 'active' : ''}
-              value={item.name}
-              onClick={handleCategoriesClick}>
-              {item.name}: {item.percentage}%
-            </StyledButton>
+      <Fragment>
+        <StyledButton
+          className={allCategoriesActive ? 'cat-all active' : 'cat-all'  }
+          value="all"
+          onClick={handleCategoriesSelectAllClick}
+        >
+          Select All
+        </StyledButton>
+        <StyledList>
+          {categories.map((item, k) => (
+            <li key={k} className={item.status ? 'active' : ''}>
+              <StyledButton
+                className={item.status ? 'active' : ''}
+                value={item.name}
+                name={item.name}
+                title={item.name}
+                percentage={Number.parseFloat(item.percentage)}
+                onClick={handleCategoriesClick}>
+                {item.name} {item.percentage}%
+              </StyledButton>
 
-          </li>
-        ))}
-      </StyledList>
+            </li>
+          ))}
+        </StyledList>
+      </Fragment>
     )
   }
+}
+
+function compare(a, b){
+  if (a.name >= b.name) return 1;
+  if (b.name < a.name) return -1;
+
+  return 0;
 }
 
 export default CategoriesControl
