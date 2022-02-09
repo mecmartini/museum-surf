74fee89dcf8993c3753829cd90b963e2f2bf4aaf
MARTINO Marco <marco.martino@ext.ec.europa.eu>
2022-02-09 13:19:40 +0100

    Update basemap

--- src/components/BaseMap/BaseMap.js
+++ src/components/BaseMap/BaseMap.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { TileLayer } from 'react-leaflet';
 
-const basemap = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
-const basemapId = 'mapbox.streets';
-const attribution = '© <a href="">Link</a>';
+const basemap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 class BaseMap extends Component {
   render() {
     return(
       <TileLayer
-        attribution={attribution}
         url={basemap}
-        id={basemapId}
       />
     )
   }
